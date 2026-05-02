e26d3128f53647a8a4f37fd6d02db23a84b40109
NGnYan <ngnyan@air-de-ngnyan.home>
2026-05-02 21:24:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// --- Active link ---
+const currentPage = location.pathname.split("/").pop() || "index.html";
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  const linkPage = link.getAttribute("href").split("/").pop();
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+});
